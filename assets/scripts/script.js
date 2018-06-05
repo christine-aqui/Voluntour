e9f99e34b50dc39bd806c8f518f0060dccb344d2
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-06-04 21:46:45 -0400

    moved videos and images folder into assets

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,18 @@
 console.log('connected');
+$('#logo').hide();
+$('.container').hide();
+$(document).ready(function () {
+  console.log("ready!");
+  setTimeout(function () {
+    $('#logo').show();
+    $('#logo').addClass('animated fadeIn');
+    setTimeout(() => {
+      $('.container').show();
+    }, 400);
+  }, 500);
+});
+// Meetup API
+// var elem = document.querySelector('#region-list');
+// var userRegion = elem.options[elem.selectedIndex].value;
+// var userRegion = $('#region-list option:selected').text();
+// console.log('User selected: ',userRegion);
