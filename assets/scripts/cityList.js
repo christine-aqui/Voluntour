cc07d418be7306d51058629997a19444614e5b86
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-06-07 00:08:30 -0400

    :sparkles: making the interface between the front end and the api

---
+++ assets/scripts/cityList.js
@@ -0,0 +1,114 @@
+const Asia = {
+  capitalCity: 'Lilongwe',
+  countryCode1:'MW',
+  countryCode2:'MWI',
+  Continent: 'Asia',
+  lat: -13.9669200,
+  lon: 33.7872500,
+}
+
+const Malawi = {
+  capitalCity: 'Lilongwe',
+  countryCode1:'MW',
+  countryCode2:'MWI',
+  Continent: 'Africa',
+  lat: -13.9669200,
+  lon: 33.7872500,
+}
+
+const Jordan = {
+  capitalCity: 'Amman',
+  countryCode1: 'JO',
+  countryCode2: 'JOR',
+  Continent: 'Asia',
+  lat: 31.9552200,
+  lon: 35.9450300,
+}
+const Nepal = {
+  capitalCity: 'Kathmandu',
+  countryCode1: 'NP',
+  countryCode2: 'NPL',
+  Continent: 'Asia',
+  lat: 27.7016900,
+  lon: 85.3206000,
+}
+const Fiji = {
+  capitalCity: 'Suva',
+  countryCode1: 'FJ',
+  countryCode2: 'FJI',
+  Continent: 'Oceania',
+  lat: -18.1416100,
+  lon: 178.4414900,
+}
+const Hawaii = {
+  capitalCity: 'Honolulu',
+  countryCode1: 'US',
+  countryCode2: 'USA',
+  Continent: 'Oceania',
+  lat: 21.289373,
+  lon: -157.917480,
+}
+const Peru = {
+  capitalCity: 'Lima',
+  countryCode1: 'PE',
+  countryCode2: 'PER',
+  Continent: 'South America',
+  lat:-12.0431800,
+  lon: -77.0282400,
+}
+const Cambodia = {
+  capitalCity: 'Phnom Penh',
+  countryCode1: 'KH',
+  countryCode2: 'KHM',
+  Continent: 'Asia',
+  lat: 11.5624500,
+  lon: 104.9160100,
+}
+const India = {
+  capitalCity: 'New Delhi',
+  countryCode1: 'IN',
+  countryCode2: 'IND',
+  Continent: 'Asia',
+  lat: 28.644800,
+  lon: 77.216721,
+}
+const Thailand = {
+  capitalCity: 'Bangkok',
+  countryCode1: 'TH',
+  countryCode2: 'THA',
+  Continent: 'Asia',
+  lat: 13.7539800,
+  lon: 100.5014400,
+}
+const Ecuador = {
+  capitalCity: 'Quito',
+  countryCode1: 'EC',
+  countryCode2: 'ECU',
+  Continent: 'South America',
+  lat: -0.2298500 ,
+  lon: -78.5249500 ,
+}
+const Kenya = {
+  capitalCity: 'Nairobi',
+  countryCode1: 'KE',
+  countryCode2: 'KEN',
+  Continent: 'Africa',
+  lat: -1.2833300,
+  lon: 36.8166700 ,
+}
+const Senegal = {
+  capitalCity: 'Dakar',
+  countryCode1: 'SN',
+  countryCode2: 'SEN',
+  Continent: 'Africa',
+  lat: 14.6937000 ,
+  lon: -17.4440600 ,
+}
+const Philippines = {
+  capitalCity: 'Manila',
+  countryCode1: 'PH',
+  countryCode2: 'PHL',
+  Continent: 'Asia',
+  lat: 14.6042000,
+  lon: 120.9822000,
+}
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,83 @@
 console.log('connected');
+var capCity = '';
+var cityCode = '';
+var cityLat = 0;
+var cityLon = 0;
+//
+//
 $('#logo').hide();
 $('.container').hide();
 $(document).ready(function () {
   console.log("document ready!");
+  //
   setTimeout(function () {
     $('#logo').show();
     $('#logo').addClass('animated fadeIn');
+    //
     setTimeout(() => {
       $('.container').show();
-      document.querySelector('#formBtn').addEventListener('click', function(e) {
+      //
+      document.querySelector('#formBtn').addEventListener('click', function (e) {
         e.preventDefault();
-
+        //
+        var userSelected = $('#region-list').val();
+        console.log('user selected: ', userSelected);
+        switch (userSelected) {
+          case "Malawi":
+            capCity = Malawi.capitalCity;
+            console.log('City name: ', capCity);
+            break;
+            //
+          case "Jordan":
+            console.log('City name: ', Jordan.capitalCity);
+            break;
+            //
+          case "Nepal":
+            console.log('City name: ', Nepal.capitalCity);
+            break;
+            //
+          case "Fiji":
+            console.log('City name: ', Fiji.capitalCity);
+            break;
+            //
+          case "Hawaii":
+            console.log('City name: ', Hawaii.capitalCity);
+            break;
+            //
+          case "Peru":
+            console.log('City name: ', Peru.capitalCity);
+            break;
+            //
+          case "Cambodia":
+            console.log('City name: ', Cambodia.capitalCity);
+            break;
+            //
+          case "India":
+            console.log('City name: ', India.capitalCity);
+            break;
+            //
+          case "Thailand":
+            console.log('City name: ', Thailand.capitalCity);
+            break;
+            //
+          case "Ecuador":
+            console.log('City name: ', Ecuador.capitalCity);
+            break;
+            //
+          case "Kenya":
+            console.log('City name: ', Kenya.capitalCity);
+            break;
+            //
+          case "Senegal":
+            console.log('City name: ', Senegal.capitalCity);
+            break;
+            //
+          case "Philippines":
+            console.log('City name: ', Philippines.capitalCity);
+            break;
+            //
+        }
+        //
         var meetupURL = `https://cors.io/?https://api.meetup.com/2/open_events?`;
         $.ajax({
           url: meetupURL,
@@ -23,9 +90,8 @@ $(document).ready(function () {
             sign: 'true',
           }
         }).done(function (response) {
-          console.log('meetup ', response);
+          // console.log('meetup ', response);
         });
-
       });
     }, 400);
   }, 500);
