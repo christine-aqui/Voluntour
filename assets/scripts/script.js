3e1351d4deff9a22ce02de26085ee70bfbecdfa7
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-06-06 21:03:33 -0400

    some changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,43 +1,50 @@
 console.log('connected');
 $('#logo').hide();
-$('.container').hide();
+// $('.container').hide();
 $(document).ready(function () {
   console.log("document ready!");
   setTimeout(function () {
     $('#logo').show();
     $('#logo').addClass('animated fadeIn');
-    setTimeout(() => {
-      $('.container').show();
-    }, 400);
+    // setTimeout(function ()  {
+    //   $('.container').show();
+    // }, 400);
   }, 500);
 });
 
 // Meetup API
-$("#formBtn").on('click', function(e) {
-  e.preventDefault();
+$(".formBtn").on("click", function  (e) {
+  e.preventDefault(); // without page resets and causes isses
+  console.log('button pressed'); // checks that button is ressed
+  var userSelLoc = $("#region-list").val(); // gets dopedown menu value
+  console.log(userSelLoc);
   // using the windows object location method
-  var urlParms = new URLSearchParams(window.location.search);
+  // var urlParms = new URLSearchParams(window.location.search);
   // using the URLSearchParams utility method
   // URLSearchParams.get() Returns the first value associated to the given search parameter.
-  var regChoi = urlParms.get('region-choice');
+  // var regChoi = urlParms.get('region-choice');
   // consol log what you got
-  console.log('User selected: ', regChoi);
+  // console.log('User selected: ', regChoi);
   //
   //
-  var meetupURL = `https://cors.io/?https://api.meetup.com/2/open_events?`;
+  var meetupURL = 'https://cors.io/?https://api.meetup.com/2/open_events?';
+
   $.ajax({
     url: meetupURL,
     method: 'GET',
     data: {
       key: '116b73e106c122e802f104e7767213',
-      city: regChoi,
-      // country: 'CA',
+      city: "Honolulu",
+      state: 'Hawaii',
+      // country: userSelLoc,
+      country: "USA",
       category: 1, //hard coded temp
       sign: 'true',
     }
   }).done(function (responce) {
-    console.log(meetupURL);
-    // console.log('meetup ', responce);
+    console.log('made it to the res');
+    // console.log(meetupURL);
+    console.log('meetup ', responce);
   });
 
 
