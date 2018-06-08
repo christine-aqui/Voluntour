97cd471d114f677bf1eb67e9662ce8da73934d67
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-06-08 00:47:35 -0400

    :recycle: changed citylist to oop for dry code

--- assets/scripts/cityList.js
+++ assets/scripts/cityList.js
@@ -1,115 +1,27 @@
-// const Asia = {
-//   capitalCity: 'Lilongwe',
-//   countryCode1: 'MW',
-//   countryCode2: 'MWI',
-//   Continent: 'Asia',
-//   lat: -13.9669200,
-//   lon: 33.7872500,
-// }
-
-const Malawi = {
-  capitalCity: 'Lilongwe',
-  countryCode1: 'MW',
-  countryCode2: 'MWI',
-  Continent: 'Africa',
-  lat: -13.9669200,
-  lon: 33.7872500,
-}
-
-const Jordan = {
-  capitalCity: 'Amman',
-  countryCode1: 'JO',
-  countryCode2: 'JOR',
-  Continent: 'Asia',
-  lat: 31.9552200,
-  lon: 35.9450300,
-}
-const Nepal = {
-  capitalCity: 'Kathmandu',
-  countryCode1: 'NP',
-  countryCode2: 'NPL',
-  Continent: 'Asia',
-  lat: 27.7016900,
-  lon: 85.3206000,
-}
-const Fiji = {
-  capitalCity: 'Suva',
-  countryCode1: 'FJ',
-  countryCode2: 'FJI',
-  Continent: 'Oceania',
-  lat: -18.1416100,
-  lon: 178.4414900,
-}
-const Hawaii = {
-  capitalCity: 'Honolulu',
-  countryCode1: 'US',
-  countryCode2: 'USA',
-  Continent: 'Oceania',
-  lat: 21.289373,
-  lon: -157.917480,
-}
-const Peru = {
-  capitalCity: 'Lima',
-  countryCode1: 'PE',
-  countryCode2: 'PER',
-  Continent: 'South America',
-  lat: -12.0431800,
-  lon: -77.0282400,
-}
-const Cambodia = {
-  capitalCity: 'Phnom Penh',
-  countryCode1: 'KH',
-  countryCode2: 'KHM',
-  Continent: 'Asia',
-  lat: 11.5624500,
-  lon: 104.9160100,
-}
-const India = {
-  capitalCity: 'New Delhi',
-  countryCode1: 'IN',
-  countryCode2: 'IND',
-  Continent: 'Asia',
-  lat: 28.644800,
-  lon: 77.216721,
-}
-const Thailand = {
-  capitalCity: 'Bangkok',
-  countryCode1: 'TH',
-  countryCode2: 'THA',
-  Continent: 'Asia',
-  lat: 13.7539800,
-  lon: 100.5014400,
-}
-const Ecuador = {
-  capitalCity: 'Quito',
-  countryCode1: 'EC',
-  countryCode2: 'ECU',
-  Continent: 'South America',
-  lat: -0.2298500,
-  lon: -78.5249500,
-}
-const Kenya = {
-  capitalCity: 'Nairobi',
-  countryCode1: 'KE',
-  countryCode2: 'KEN',
-  Continent: 'Africa',
-  lat: -1.2833300,
-  lon: 36.8166700,
-}
-const Senegal = {
-  capitalCity: 'Dakar',
-  countryCode1: 'SN',
-  countryCode2: 'SEN',
-  Continent: 'Africa',
-  lat: 14.6937000,
-  lon: -17.4440600,
-}
-const Philippines = {
-  capitalCity: 'Manila',
-  countryCode1: 'PH',
-  countryCode2: 'PHL',
-  Continent: 'Asia',
-  lat: 14.6042000,
-  lon: 120.9822000,
-}
-
+class CityObj {
+  constructor(capitalCity, countryCode1, countryCode2, state, Continent, lat, lon) {
+    this.capitalCity = capitalCity;
+    this.countryCode1 = countryCode1;
+    this.countryCode2 = countryCode2;
+    this.state = state;
+    this.Continent = Continent;
+    this.lat = lat;
+    this.lon = lon;
+  }
+};
+//
+const malawi = new CityObj('Lilongwe', 'MW', 'MWI', '', 'Africa', -13.9669200, 33.7872500);
+const jordan = new CityObj('Amman', 'JO', 'JOR', '', 'Asia', 31.9552200, 35.9450300);
+const nepal = new CityObj('Kathmandu', 'NP', 'NPL', '', 'Asia', 27.7016900, 85.3206000);
+const fiji = new CityObj('Suva', 'FJ', 'FJI', '', 'Oceania', -18.1416100, 178.4414900);
+const hawaii = new CityObj('Honolulu', 'US', 'USA', 'HI', 'Oceania', 21.289373, -157.917480);
+const peru = new CityObj('Lima', 'PE', 'PER', '', 'South America ',-12.0431800, -77.0282400);
+const cambodia = new CityObj('Phnom Penh', 'KH', 'KHM', '', 'Asia',11.5624500, 104.9160100);
+const india = new CityObj('New Delhi', 'IN', 'IND', '', 'Asia', 28.644800, 77.216721);
+const thailand = new CityObj('Bangkok', 'TH', 'THA', '', 'Asia', 13.7539800, 100.5014400);
+const Ecuador = new CityObj('Quito', 'EC', 'ECU', '', 'South America ',-0.2298500, -78.5249500);
+const kenya = new CityObj('Nairobi', 'KE', 'KEN', '', 'Africa', -1.2833300, 36.8166700);
+const senegal = new CityObj('Dakar', 'SN', 'SEN', '', 'Africa', 14.6937000, -17.4440600);
+const philippines = new CityObj('Manila', 'PH', 'PHL', '', 'Asia', 14.6042000, 120.9822000);
+//
+// console.log('this is malai: ', malawi);
