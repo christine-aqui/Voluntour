e659ea797a6d22e92f29f0ffe927f323d495ae4a
shrekstah <ahmed.gamal.m@gmail.com>
2018-06-09 12:39:33 -0400

    second push for Sat. APIs configuered, ready to nest to HTML

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -121,8 +121,9 @@ $(document).ready(function () {
             // takes response string and converts it to a JavaScript object
             let myResp = JSON.parse(response);
             console.log('meetup: ', myResp);
-                      // }
+            // }
           });
+
           let newsKey = 'd91452c6c0c0450dbd402a43d19d7905'
           let newsURL = `https://newsapi.org/v2/everything?q=${userSelected}&apikey=${newsKey}`
           $.ajax({
@@ -130,9 +131,20 @@ $(document).ready(function () {
             method: 'GET',
             language: 'en',
             sortBy: 'relevancy',
-
           }).done(function(newsResponse){
             console.log('news: ', newsResponse)
+          })
+
+          let advisoryKey = '7csv43cjcwbtnfyspr6n6gjd';
+          let advisoryURL = `https://api.tugo.com/v1/travelsafe/countries/${cityCode}`;
+          $.ajax({
+            url: advisoryURL,
+            method: 'GET',
+            headers: {
+              "X-Auth-API-Key": advisoryKey
+            }
+          }).done(function(advisoryResponse){
+            console.log('advisory: ', advisoryResponse)
           })
 
         } else{
