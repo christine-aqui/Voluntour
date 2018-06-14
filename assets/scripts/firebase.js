7353673e1bbe24a76fc49b8f11cf49097f481e06
shrekstah <ahmed.gamal.m@gmail.com>
2018-06-14 18:58:03 -0400

    final commit

--- assets/scripts/firebase.js
+++ assets/scripts/firebase.js
@@ -193,13 +193,12 @@
   //
   function firebaseDisplay() {
     return `
-      <div class='rTrip hvr-underline-from-center'>
-        <img class="orgImage" src="${itineraryImage}" alt="">
-        <div class="groupFire">
-        <a href="${fireLine}"><h5>${fireOrg}</h5></a>
-        <h5>Look at charitable tours in ${fireRegion}</h5>
-        <h5>${fireType}</h5>
-        </div>
-      </div>
+      <div class='hvr-underline-from-center'>
+      <div class="media">
+<img class="mr-3 orgImage" src="${itineraryImage}" alt="Organization Logo">  <div class="media-body">
+    <a href="${fireLine}"><h5 class="mt-0">${fireOrg}</h5></a>
+    <span><h5>Look at charitable tours in ${fireRegion}</h5>
+    <h5>${fireType}</h5></span>
+  </div>
     `;
   };
